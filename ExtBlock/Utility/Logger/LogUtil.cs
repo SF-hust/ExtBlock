237c4985959ed2055e578550f964e1a9f322ee98
Zhao Yi <1609957690@qq.com>
2022-10-14 13:36:45 +0800

    add a simple log system

---
+++ ExtBlock/Utility/Logger/LogUtil.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtBlock.Utility.Logger
+{
+    public static class LogUtil
+    {
+        public static LoggerWarp Logger = new LoggerWarp(null);
+        public static ILogger GetLogger()
+        {
+            return Logger;
+        }
+    }
+}
---
+++ ExtBlock/Utility/Logger/LoggerWarp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtBlock.Utility.Logger
+{
+    public class LoggerWarp : ILogger
+    {
+        private ILogger? _logger;
+
+        public void SetLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public LoggerWarp(ILogger? logger)
+        {
+            _logger = logger;
+        }
+
+        public void Trace(string message)
+        {
+            _logger?.Trace(message);
+        }
+
+        public void Info(string message)
+        {
+            _logger?.Info(message);
+        }
+
+        public void Debug(string message)
+        {
+            _logger?.Debug(message);
+        }
+
+        public void Warn(string message)
+        {
+            _logger?.Warn(message);
+        }
+
+        public void Error(string message)
+        {
+            _logger?.Error(message);
+        }
+    }
+}
