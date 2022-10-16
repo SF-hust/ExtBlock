4abb09a336d86887c85e5c38e03b717763649f46
Zhao Yi <1609957690@qq.com>
2022-10-16 15:52:40 +0800

    Revert "."
    This reverts commit 54389edadbb21f96b8c1e046ed97d3667bf5c2bd.

--- ExtBlockServer/Server.cs
+++ ExtBlockServer/Server.cs
@@ -1,32 +1,10 @@
-﻿using ExtBlock;
-using ExtBlock.Core;
-using ExtBlock.Core.Registry;
-using ExtBlock.CoreMod.Blocks;
-using ExtBlock.Game;
-using ExtBlock.Server;
-using ExtBlock.Utility.Logger;
-
-namespace ExtBlockServer
+﻿namespace ExtBlockServer
 {
     public class Server
     {
-        private readonly static ILogger logger = LogUtil.GetLogger();
         static void Main(string[] args)
         {
-            LogUtil.Logger.SetLogger(new ConsoleLogger());
-            logger.Info("Server start runing...");
-            try
-            {
-                Blocks.Init();
-                Registries.InitInternalRegistries();
-                Registries.FireRootRegisterEvent();
-                Registries.FireRegisterEvents();
-                logger.Info("Server terminate.");
-            }
-            catch (Exception e)
-            {
-                logger.Error(e.Message);
-            }
+            Console.WriteLine("Hello, World!");
         }
     }
 }
